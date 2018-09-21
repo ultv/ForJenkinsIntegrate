3b2ac50d5881640d3ef30157cc09a71b0720e412
ultv <ultv@inbox.ru>
2018-09-21 23:22:48 +0400

    init commit

---
+++ MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForJenkinsIntegrate
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Проверяем сборку в Jenkins");
+        }
+        
+    }
+
+    public class Calc
+    {
+        public int Summ(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ForJenkinsIntegrate;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSumm_5_and_3_returned_8()
+        {
+            Calc calc = new Calc();
+            int act = calc.Summ(5, 3);
+
+            int expected = 8;
+
+            Assert.AreEqual(act, expected);
+        }
+
+        [TestMethod]
+        public void TestSub_5_and_3_returned_2()
+        {
+            Calc calc = new Calc();
+            int act = calc.Sub(5, 3);
+
+            int expected = 2;
+
+            Assert.AreEqual(act, expected);
+        }
+    }
+}
